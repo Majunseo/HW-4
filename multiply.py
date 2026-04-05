3ec2776315feecd367ccabfce4bf39c446d1f4d8
Majunseo <junseo3677@gmail.com>
2026-04-05 22:32:24 +0900

    fix issue #3

--- multiply.py
+++ multiply.py
@@ -6,6 +6,9 @@ def times(number_list):
 
 def main():
     numbers = input("곱할 숫자들을 공백으로 구분하여 입력하세요: ")
+    if numbers.strip() == "":
+        print("입력값이 비어 있습니다.")
+        return
     number_list = map(float, numbers.split())
     result = times(number_list)
     print(result, "입니다")
